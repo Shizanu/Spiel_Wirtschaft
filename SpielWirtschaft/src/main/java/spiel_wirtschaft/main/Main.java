1726ec023e2eb08c016d4bb84b2fe51ed5ce7d6c
Shizanu <tobias.rojahn@gmx.net>
2017-09-30 16:40:34 +0200

    java fx

--- SpielWirtschaft/src/main/java/spiel_wirtschaft/main/Main.java
+++ SpielWirtschaft/src/main/java/spiel_wirtschaft/main/Main.java
@@ -1,11 +1,20 @@
 package spiel_wirtschaft.main;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class Main {
+public class Main extends Application {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 }
