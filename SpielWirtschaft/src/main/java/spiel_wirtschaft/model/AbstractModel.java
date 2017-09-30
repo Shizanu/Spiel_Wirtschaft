a8383fac3e5d47c173e11928ea130e2b637d4a4e
Shizanu <tobias.rojahn@gmx.net>
2017-09-30 15:44:44 +0200

    Introduced apache util and reflection equal/hashcode/toString

--- SpielWirtschaft/src/main/java/spiel_wirtschaft/model/AbstractModel.java
+++ SpielWirtschaft/src/main/java/spiel_wirtschaft/model/AbstractModel.java
@@ -1,5 +1,9 @@
 package spiel_wirtschaft.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
 public abstract class AbstractModel {
 
 	private final long ID;
@@ -11,6 +15,21 @@ public abstract class AbstractModel {
 
 	public long getID() {
 		return ID;
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
 	}
 
 }
