54cffa5b2f0f05280703ad4e460aed715023f8bc
AnneRojahn <anne.rojahn@gmx.net>
2017-10-01 13:30:05 +0200

    -

--- SpielWirtschaft/src/main/java/spiel_wirtschaft/controller/LoadSaveCtrl.java
+++ SpielWirtschaft/src/main/java/spiel_wirtschaft/controller/LoadSaveCtrl.java
@@ -12,39 +12,30 @@ import spiel_wirtschaft.model.City;
 public class LoadSaveCtrl extends AbstractController {
 
 	public void save(City city) {
-
 		try {
 			File file = new File("C:\\Dateien\\Spiel_Wirtschaft\\Spielstand\\Spielstand1.xml");
 			JAXBContext jaxbContext = JAXBContext.newInstance(City.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
-			// output pretty printed
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
 			jaxbMarshaller.marshal(city, file);
-			jaxbMarshaller.marshal(city, System.out);
-
 		} catch (JAXBException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-
 	}
 
 	public City load() {
-
 		try {
-
 			File file = new File("C:\\\\Dateien\\\\Spiel_Wirtschaft\\\\Spielstand\\\\Spielstand1.xml");
 			JAXBContext jaxbContext = JAXBContext.newInstance(City.class);
 
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 			City city = (City) jaxbUnmarshaller.unmarshal(file);
-			System.out.println(city);
 			return city;
 
 		} catch (JAXBException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		return null;
 	}
 }
