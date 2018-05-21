f3bc8a70cc4efccc068fa658b6d2aa8cdf6f30dc
Anne Rojahn <Anne Rojahn@DESKTOP-AG2GCCJ.Speedport_W_724V_09011603_05_013>
2018-05-21 12:17:48 +0200

    Test überarbeitet

--- SpielWirtschaft/src/test/java/spiel_wirtschaft/controller/LoadSaveCtrlTest.java
+++ SpielWirtschaft/src/test/java/spiel_wirtschaft/controller/LoadSaveCtrlTest.java
@@ -3,7 +3,10 @@ package spiel_wirtschaft.controller;
 import org.junit.Assert;
 import org.junit.Test;
 
+import spiel_wirtschaft.model.GebaeudeEnum;
+import spiel_wirtschaft.model.KartenKoordinatenBE;
 import spiel_wirtschaft.model.SpielBE;
+import spiel_wirtschaft.model.StadtBE;
 import spiel_wirtschaft.test.AbstractTestBase;
 import spiel_wirtschaft.test.TestObjectFactory;
 
@@ -14,6 +17,11 @@ public class LoadSaveCtrlTest extends AbstractTestBase {
 	@Test
 	public void testSaveLoad() {
 		SpielBE testSpiel = TestObjectFactory.createBasicSpiel();
+		KartenKoordinatenBE kartenkoordinaten = new KartenKoordinatenBE();
+		kartenkoordinaten.init(1, 1);
+		StadtBE neueStadt = new StadtBE(10, "Neustadt", kartenkoordinaten);
+		neueStadt.addGebaeude(GebaeudeEnum.MARKTPLATZ);
+		testSpiel.getSpielkarte().getStaedte().add(neueStadt);
 		loadSaveCtrl.save(testSpiel);
 		SpielBE geladenesSpiel = loadSaveCtrl.load();
 		Assert.assertEquals(testSpiel, geladenesSpiel);
